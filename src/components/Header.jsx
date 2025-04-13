3a1dd7238345af609c02fda26262f0367fd07c40
Arnold <Arnold>
2025-04-13 14:29:38 +0300

    complete header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='Header'>
+        <h1>Expense Tracker</h1>
+        <p>Start taking control of your finances by categorizing and analyzng your spending</p>
+    </div>
+  )
+}
+
+export default Header
